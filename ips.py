c43d4c24cd2750d0118277d6253d2759a364da9f
Sumit Anand <anandsumit2000@gmail.com>
2024-01-27 20:43:21 +0530

    Optimizing and refactoring the code

--- ips.py
+++ ips.py
@@ -3,100 +3,63 @@ import boto3
 import ipaddress
 from typing import List
 
-USAGE = """
-**USAGE**
-python describe_unused_ips.py {subnet-id} [lb]
-"""
 
 ec2_client = boto3.client("ec2")
 
 
-def main(subnet_id: str, mode: str):
-    print(f"{subnet_id=} {mode=}")
+def main(subnet_id: str):
+    print(f"{subnet_id=}")
 
     # get cidr ips
-    cidr = get_cidr(subnet_id=subnet_id)
-    cidr_ips = [str(ip) for ip in ipaddress.IPv4Network(cidr)]
-    cidr_ips.sort(key=ipaddress.IPv4Address)
+    cidr = get_cidr(subnet_id)
+    cidr_ips = sorted(map(str, ipaddress.IPv4Network(cidr)))
 
     # extract reserved ips
-    #  The first four IP addresses and the last IP address in each subnet CIDR block
-    #  are not available for you to use, and cannot be assigned to an instance.
     reserved_ips = cidr_ips[0:4] + [cidr_ips[-1]]
-    reserved_ips.sort(key=ipaddress.IPv4Address)
 
     # get used ips
-    used_ips = get_used_ips(subnet_id=subnet_id)
-    used_ips.sort(key=ipaddress.IPv4Address)
+    used_ips = get_used_ips(subnet_id)
+    used_ips.sort()
 
     # extract unused ips
-    unused_ips = list(set(cidr_ips) - set(used_ips) - set(reserved_ips))
-    unused_ips.sort(key=ipaddress.IPv4Address)
+    unused_ips = sorted(set(cidr_ips) - set(used_ips) - set(reserved_ips))
 
     # output
     print(f"{cidr=}")
-    print_list("cidr_ips", cidr_ips, mode)
+    print_list("cidr_ips", cidr_ips)
     print("-----------")
-    print_list("reserved_ips", reserved_ips, mode)
+    print_list("reserved_ips", reserved_ips)
     print("-----------")
-    print_list("used_ips", used_ips, mode)
+    print_list("used_ips", used_ips)
     print("-----------")
-    print_list("unused_ips", unused_ips, mode)
+    print_list("unused_ips", unused_ips)
     print("-----------")
-    print("cidr={} cidr_ips={} reserved={} used={} unused={}".format(
-        cidr,
-        len(cidr_ips), len(reserved_ips), len(used_ips), len(unused_ips)
-    ))
+    print(f"cidr={cidr} cidr_ips={len(cidr_ips)} reserved={len(reserved_ips)} used={len(used_ips)} unused={len(unused_ips)}")
 
 
-def print_list(label: str, target_list: List, mode: str):
-    if mode == "lb":
-        print(f"{label}=")
-        for t in target_list:
-            print(f"{t}")
-    else:
-        print(f"{label}={target_list}")
+def print_list(label: str, target_list: List):
+    print(f"{label}=")
+    for t in target_list:
+        print(f"{t}")
 
 
 def get_cidr(subnet_id: str) -> str:
-    """
-    Get Subnet CIDR
-    :param subnet_id:VPC subnet-id
-    :return: The IPv4 CIDR block
-    """
-    paginator = ec2_client.get_paginator('describe_subnets')
-    for subnets in paginator.paginate(SubnetIds=[subnet_id]):
-        for subnet in subnets["Subnets"]:
-            return subnet['CidrBlock']
-    raise Exception(f"{subnet_id=} has no CidrBlock.")
+    response = ec2_client.describe_subnets(SubnetIds=[subnet_id])
+    return response['Subnets'][0]['CidrBlock']
 
 
 def get_used_ips(subnet_id: str) -> List[str]:
-    """
-    Get used ips from NetworkInterfaces(primary and secondary private IP address)
-    :param subnet_id:
-    :return: [IP address, ...]
-    """
-    private_ips = []
-    paginator = ec2_client.get_paginator('describe_network_interfaces')
-    for nw_ifs in paginator.paginate(
-            Filters=[{'Name': 'subnet-id', 'Values': [subnet_id]}, ]
-    ):
-        for nw_if in nw_ifs["NetworkInterfaces"]:
-            ips = [ip["PrivateIpAddress"] for ip in nw_if['PrivateIpAddresses']]
-            private_ips.extend(ips)
+    response = ec2_client.describe_network_interfaces(Filters=[{'Name': 'subnet-id', 'Values': [subnet_id]}])
+    private_ips = [ip['PrivateIpAddress'] for nw_if in response.get('NetworkInterfaces', []) for ip in nw_if.get('PrivateIpAddresses', [])]
     return private_ips
 
 
 if __name__ == '__main__':
     args = sys.argv
     if len(args) == 2:
-        main(subnet_id=args[1], mode="normal")
+        main(subnet_id=args[1])
         sys.exit(0)
-    elif len(args) == 3:
-        main(subnet_id=args[1], mode=args[2])
-        sys.exit(0)
+
     else:
         print("incorrect argument")
-        print(USAGE)
         sys.exit(1)
